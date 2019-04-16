e6835ce557866c7a60932aeb726af27d601d24c5
MohamedGazya <mohamed.gazya@student.guc.edu.eg>
2019-04-16 09:12:41 +0200

    Milestone 1 network protocols

--- app/src/main/java/com/example/thermonitor/DeviceList.java
+++ app/src/main/java/com/example/thermonitor/DeviceList.java
@@ -9,10 +9,12 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,50 +29,53 @@ public class DeviceList extends AppCompatActivity {
 
     private WifiManager wifiManager;
     private ListView mListView;
-    private int size = 0;
     private List <ScanResult> results;
     private ArrayList<String> esps = new ArrayList<>();
+    private ArrayList<String> ssid = new ArrayList<>();
     private String espinfo;
-
+    private Button scanButton;
 
     int espimage = R.drawable.esp;
 
-
-    public DeviceList() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-       wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
+        if (!wifiManager.isWifiEnabled()){
 
-
-
-       if (!wifiManager.isWifiEnabled()){
-
-           Toast.makeText(this, "WiFi is disabled.... we will to enable it for you!!!", Toast.LENGTH_LONG).show();
-           wifiManager.setWifiEnabled(true);
-       }
-
+            Toast.makeText(this, "WiFi is disabled.... we will to enable it for you!!!", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
         scanWifi();
-        mListView = findViewById(R.id.OS);
+
+        scanButton = findViewById(R.id.scan);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanWifi();
+            }
+        });
+
+        mListView = findViewById(R.id.esp);
 
         CustomAdaptor customAdaptor = new CustomAdaptor();
+        Log.d("ggg", esps.get(0));
         mListView.setAdapter(customAdaptor);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(DeviceList.this, DeviceDetail.class);
-                intent.putExtra("esp", results.get(position).SSID);
+                intent.putExtra("esp", ssid.get(position));
                 startActivity(intent);
+
             }
         });
 
     }
-
     class CustomAdaptor extends BaseAdapter{
 
         @Override
@@ -102,8 +107,9 @@ public class DeviceList extends AppCompatActivity {
 
     private void scanWifi(){
         esps.clear();
-        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         wifiManager.startScan();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
         Toast.makeText(this, "Scanning for nearby  WiFi devices", Toast.LENGTH_SHORT).show();
     }
 
@@ -112,13 +118,14 @@ public class DeviceList extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             results = wifiManager.getScanResults();
             unregisterReceiver(this);
-            for(int i=0;i<results.size();i++){
-                if(results.get(i).SSID.equals("Nadeem")){
-                    espinfo ="SSID:  " + results.get(i).SSID +"      " + "MAC Address:  " + results.get(i).BSSID;
-                    esps.add(espinfo);
+            for (int i = 0; i < results.size(); i++) {
+                if(results.get(i).SSID.toString().equals("khaledgazya")){
+                espinfo = "SSID:  " + results.get(i).SSID.toString() + "      " + "MAC Address:  " + results.get(i).BSSID.toString();
+                esps.add(espinfo);
+                ssid.add(results.get(i).SSID.toString());
                 }
             }
+
         }
     };
-
 }
